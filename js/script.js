c299009457047721fe7ef3926075f2335a05e0d3
Kathryn Mersmann <kathrynmersmann@mwc-225135.dhcp.missouri.edu>
2015-01-27 19:54:32 -0600

    simple ajax call with basebalcard json

--- js/script.js
+++ js/script.js
@@ -1,9 +1,38 @@
+
+
+var playerData;
+
 $(document).ready(function() {
     console.log("Hello world.")
+    getData();
 });
 
 
+var array = ["string1", "string2"];
 
+var obj ={};
 
+var obj2 = {
+	"name" : "Katy",
+	"things I like" : ["food", "some people"]
+};
 
+function getData() {
+	$.getJSON("js/mersmann_baseballcard.json", function(data) {
+		console.log(data);
+		playerData = data;
 
+		drawStuff();
+
+});
+}
+
+function drawStuff() {
+
+	$(".chart").html(playerData.name);
+
+	$.each(playerData.stats, function(i, item) {
+		$(".chart").append("<p>"+item.AVG+"</p>");
+	});
+
+}
